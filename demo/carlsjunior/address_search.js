9737b40ef97e18b9543222a104cfbb2fc2561307
demonstration-spectrum <demonstration.spectrum@gmail.com>
2017-10-03 16:50:02 +1100

    Add files via upload

---
+++ demo/carlsjunior/address_search.js
@@ -0,0 +1,58 @@
+function init_inputsearch(){  //https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
+		var input = document.getElementById('pac-input');
+		var searchBox = new google.maps.places.SearchBox(input);
+
+		// Bias the SearchBox results towards current map's viewport.
+		map.addListener('bounds_changed', function() {
+			  searchBox.setBounds(map.getBounds());
+			});
+		var markers = [];
+		searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+          }
+
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url:  "https://demonstration-spectrum.github.io/Logos/Maps Pin Place.ico", //place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location,
+			  //label: "address here it is!"
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
+	
+	
+}
---
+++ demo/carlsjunior/reset_layer.js
@@ -0,0 +1,144 @@
+function reset_layer(){
+	layers = layer_pointer();
+	
+	for (var key in layers){
+		console.log(key);
+		var list_default = list_of_class[key]['default'];
+		var input_check = false;
+		if(list_default == 'checked'){
+			input_check = true;
+		}
+		if(typeof(list_of_class[key]['ulid']) != 'undefined'){
+			var input_list = document.querySelectorAll("input[querytype='"+key+"']");
+			for (var i = 0; i < input_list.length; i++){
+				var input = input_list[i];
+				if(input_check == true){
+					$(input).prop("checked",true);
+				}else{
+					$(input).prop("checked",false);
+				}
+			}
+			setQuery(key);
+			checkall_start();			
+		} else {
+			if (input_check == true){
+				layers[key].setMap(map);
+			} else {
+				layers[key].setMap(null);
+			}
+		}
+
+		//show_pc_outline();	
+	}
+	
+	
+}
+
+$("input[id='checkall']").change(function() {
+   	var ulid = $(this).attr('ul');
+	//alert($(this).text().toLowerCase());
+	if ($(this).prop("checked")==true) {
+		$("ul[id='"+ulid+"']").find("input[type=checkbox]").prop('checked',true);
+	}else{
+
+		$("ul[id='"+ulid+"']").find("input[type=checkbox]").prop('checked',false);	
+	}
+	var querytype_list = [ulid.replace("_list",""),ulid.replace("_list","_1"),ulid.replace("_list","_2"),ulid.replace("_list","_3"),ulid.replace("_list","_4"),ulid.replace("_list","_5"),ulid.replace("_list","_6"),ulid.replace("_list","_7"),ulid.replace("_list","_8"),ulid.replace("_list","_9"),ulid.replace("_list","_10"),ulid.replace("_list","_11"),ulid.replace("_list","_12"),ulid.replace("_list","_13"),ulid.replace("_list","_14"),ulid.replace("_list","_15")];
+
+	for (var i=0; i<querytype_list.length; i++){
+		var querytype = querytype_list[i];
+		if (typeof(list_of_class[querytype]) != "undefined"){
+			setQuery(querytype);
+		}
+	}
+	//alert(querytype_list + "<br>");
+	
+});
+
+
+
+function setQuery_xml(querytype){
+	//querytype = "comp";
+	var layer = layer_pointer()[querytype];
+	for (var i=0; i < layer.docs[0].placemarks.length; i++) {
+		var query_val = layer.docs[0].placemarks[i].name;
+		//alert(i+": "+ query_val);
+		var input = document.querySelector("input[query_val='"+query_val+"']");
+		if (!input){
+			alert("found an null");
+			alert(query_val);
+		}else{
+			if (input.checked == true){
+				//alert('true');
+				layer.docs[0].placemarks[i].marker.setMap(map);
+			} else{
+				//alert('false');
+				layer.docs[0].placemarks[i].marker.setMap(null);
+			}
+		}
+        
+	}
+	map_ordering();
+}
+
+function hide_afterparse(){
+	//var geoXmlDoc = doc[0];
+	//alert(geoXmlDoc.placemarks.length);
+	for (var i=0; i < competitor_layer.docs[0].placemarks.length; i++) {
+		competitor_layer.docs[0].placemarks[i].marker.setMap(map);
+	}
+    alert('parse'); 
+	//competitor_layer.hideDocument();
+}
+
+//$("#checkall").click(
+//setInterval(hide_afterparse,3000);
+
+function checkall_start(){
+	checkall_list = document.querySelectorAll("input[id='checkall']");
+	//alert(checkall_list.length);
+	for (var i = 0; i< checkall_list.length; i++){
+			var checkall= checkall_list[i];
+			//alert(checkall);
+			var ulid = checkall.getAttribute('ul');
+			var querytype_list = [ulid.replace("_list",""),ulid.replace("_list","_1"),ulid.replace("_list","_2")];
+			for (var j=0; j<querytype_list.length; j++){
+				var querytype = querytype_list[j];
+				if (typeof(list_of_class[querytype]) != "undefined"){
+					if(list_of_class[querytype]['default'] == 'checked'){
+						if($(checkall).parent().prop("class").indexOf("toggle") >= 0){
+							$(checkall).parent().prop("class","toggle btn btn-default");
+						}
+						checkall.checked = true;
+						console.log(checkall.getAttribute('ul') + checkall.checked);
+					}else{
+						if($(checkall).parent().prop("class").indexOf("toggle") >= 0){
+							$(checkall).parent().prop("class","toggle btn btn-default off");
+						}						
+
+						checkall.checked = false;
+					}
+				}
+			}
+	}
+}
+
+checkall_start();
+function hide_pc_outline(){
+	blank_pc_layer1.setMap(null);
+
+}
+
+function show_pc_outline(){
+	blank_pc_layer1.setMap(map);
+
+}	
+
+$(".zoom_button").click(function(){
+	if ($(this).text().indexOf("In")>=0){
+		map.setZoom(map.getZoom()+1);
+	}else if (map.getZoom() > 0) {
+		map.setZoom(map.getZoom()-1);
+	}
+	
+});
