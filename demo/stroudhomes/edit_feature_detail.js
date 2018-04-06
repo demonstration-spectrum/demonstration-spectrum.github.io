3a150f8aeca9af4f2b12d147449ab1f025ec7521
demonstration-spectrum <demonstration.spectrum@gmail.com>
2018-04-06 17:30:49 +1000

    Update edit_feature_detail.js

--- demo/stroudhomes/edit_feature_detail.js
+++ demo/stroudhomes/edit_feature_detail.js
@@ -1,63 +1,29 @@
+var $nav = $("#main-nav-tabs");
+var $nav_ul = $nav.children("ul[class='nav nav-tabs']");
+$nav_ul.children("li").removeClass("active");
+var $edit_nav = $nav_ul.children("li[id='edit-tab-nav']");
+$edit_nav.addClass("active");
+
+var pane_id;
+pane_id = $edit_nav.children("a").attr('href');
+var $nav_pane = $nav.children(".tab-content");
+$nav_pane.children("div").removeClass("active");
+var $edit_pane = $nav_pane.children(pane_id);
+$edit_pane.addClass("active");
+var $edit_fields = $edit_pane.find("#edit_fields");
+var $submit = $nav_pane.find("button[id='edit_submit']");
+
+
 function edit_feature(verbose_name, tableId, list_pointer, base_id_col, id_col, id_val, field_list){
-	var $nav = $("#main-nav-tabs");
-	var $nav_ul = $nav.children("ul[class='nav nav-tabs']");
-	$nav_ul.children("li").removeClass("active");
-	$edit_nav = $nav_ul.children("li[id='edit-tab-nav']");
-	$edit_nav.addClass("active");
-
-	var pane_id;
-	pane_id = $edit_nav.children("a").attr('href');
-	$nav_pane = $nav.children(".tab-content");
-	$nav_pane.children("div").removeClass("active");
-	$edit_pane = $nav_pane.children(pane_id);
-	$edit_pane.addClass("active");
-	$edit_fields = $edit_pane.find("#edit_fields");
-
 	$edit_fields.empty();
-	$submit = $nav_pane.find("button[id='edit_submit']");
 	for (i=0; i< field_list.length; i++){
 		$edit_fields.append($format_row(field_list[i]));
 	}
-	var input_id_string = '<input type="hidden" class="id_col" value="{ID_VALUE}" name="{ID_COL}">'.replace(/{ID_COL}/g, base_id_col).replace(/{ID_VALUE}/g, id_val);
+	var input_id_string = '<input type="hidden" class="id_col" value="{ID_VALUE}" name="{ID_COL}" data-tableid = "{TABLEID}" data-list_pointer = "{list_pointer}">'.replace(/{ID_COL}/g, base_id_col).replace(/{ID_VALUE}/g, id_val).replace(/{TABLEID}/g, tableId).replace(/{list_pointer}/g, list_pointer);
+
 	$edit_fields.append($(input_id_string));
 	console.log($(input_id_string).prop("outerHTML"));
 	console.log(input_id_string);
-	$submit.on("click", function(){
-		$(this).button('loading');
-		console.log($(this));
-		var url =  "https://spectrum-map.herokuapp.com/fusion/stroudhomes.spectrum.data/";
-		
-		var $edit_fields = $(this).parent().parent().parent().parent().find("#edit_fields");
-		var $id_field = $edit_fields.find('input[class="id_col"]');
-		var set_array = [];
-		
-		var query = "UPDATE " + tableId + " SET {SET_FIELDS} Where {ID_COL} = '{ID_VALUE}' ".replace(/{ID_COL}/g, base_id_col).replace(/{ID_VALUE}/g, id_val);
-		$input_fields = $edit_fields.find("input").not($id_field);
-		console.log($edit_fields);
-		$input_fields.each(function(){
-			console.log('set array: ' + $(this).prop("name"));
-			set_array.push("{COL_NAME} = '{COL_VALUE}'".replace(/{COL_NAME}/g,$(this).prop("name")).replace(/{COL_VALUE}/g,$(this).prop("value")));
-		});
-		set_array = set_array.join(", ");
-		query = query.replace(/{SET_FIELDS}/g, set_array);
-
-		console.log('query: ' + query);
-		
-		query = encodeURIComponent(query);		
-		dataType = "application/json";
-		$.get({
-		  type: "GET",
-		  url: url + query,
-		  success: function (data) {
-						console.log(data);
-						var layer = layer_pointer()[list_pointer];
-						var layer_query = layer.query
-						layer.setOptions({query: layer_query});
-						$submit.button('reset');
-					},
-		});
-		
-	});
 }
 
 function $format_row(field){
@@ -123,3 +89,48 @@ function alter_infohtml(e, tableId, base_id_col) {
 		
 		
 	}
+	
+
+$submit.on("click", function(){
+	//$(this).button('loading');
+	var $edit_fields = $(this).parent().parent().parent().parent().find("#edit_fields");
+	//console.log(" $edit_fields" +  $edit_fields);
+	var $id_field = $edit_fields.find('input[class="id_col"]');
+	var base_id_col = $id_field.prop("name");
+	var id_val = $id_field.prop("value");
+	var tableId = $id_field.attr("data-tableid"); //have to use attr here, not prop
+	var list_pointer = $id_field.attr("data-list_pointer");
+	//console.log($(this));
+	var url =  "https://spectrum-map.herokuapp.com/fusion/stroudhomes.spectrum.data/";
+	
+	
+	
+	var set_array = [];
+	
+	var query = "UPDATE " + tableId + " SET {SET_FIELDS} Where {ID_COL} = '{ID_VALUE}' ".replace(/{ID_COL}/g, base_id_col).replace(/{ID_VALUE}/g, id_val);
+	var $input_fields = $edit_fields.find("input").not($id_field);
+	console.log($edit_fields);
+	$input_fields.each(function(){
+		//console.log('set array: ' + $(this).prop("name"));
+		set_array.push("{COL_NAME} = '{COL_VALUE}'".replace(/{COL_NAME}/g,$(this).prop("name")).replace(/{COL_VALUE}/g,$(this).prop("value")));
+	});
+	set_array = set_array.join(", ");
+	query = query.replace(/{SET_FIELDS}/g, set_array);
+
+	console.log('query: ' + query);
+	$submit.button('reset');
+	query = encodeURIComponent(query);		
+	dataType = "application/json";
+	$.get({
+	  type: "GET",
+	  url: url + query,
+	  success: function (data) {
+					console.log(data);
+					var layer = layer_pointer()[list_pointer];
+					var layer_query = layer.query
+					layer.setOptions({query: layer_query});
+					$submit.button('reset');
+				},
+	});
+	
+});	
